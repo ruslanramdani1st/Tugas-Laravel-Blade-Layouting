6e68d5991e64e72bed502d3f1123623fcb091b1e
ruslanramdani1st <ruslanramdani1st@gmail.com>
2021-10-14 17:06:39 +0700

    Belajar Menghubungkan database dan membuat model dan controller

--- resources/views/layouts/admin/book/index.blade.php
+++ resources/views/layouts/admin/book/index.blade.php
@@ -15,6 +15,63 @@ Nama buku : {{ $book->nama_buku}} <br>
 
 Jumlah buku : {{ $book->jumlah_halaman}} <br>
 
-Translate Judul buku : {{ $book->translate_judul_buku}} <br> <br>
+Translate Judul buku : {{ $book->translate_judul_buku ?? 'Belum ada translate'}} <br> <hr>
 @endforeach
 @endsection
+
+{{--
+@extends('layouts.master')
+
+@section('title', 'Halaman User')
+
+@section('section')
+<!-- Content Header (Page header) -->
+<section class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1>Halaman Daftar Buku</h1>
+            </div>
+        </div>
+    </div><!-- /.container-fluid -->
+</section>
+
+<!-- Main content -->
+<section class="content">
+
+    <!-- Default box -->
+    <div class="card">
+        <div class="card-header">
+            <h3 class="card-title">Title</h3>
+            <div class="card-tools">
+                <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                    <i class="fas fa-minus"></i>
+                </button>
+                <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
+                    <i class="fas fa-times"></i>
+                </button>
+            </div>
+        </div>
+        <div class="card-body">
+            @foreach ($buku as $data)
+            Nama Buku : {{ $data->nama_buku }} <br>
+            @if($data->jumlah_halaman > 1)
+            Jumlah Halaman Buku : {{ $data->jumlah_halaman }} <br>
+            @else
+            Jumlah Halaman Buku : <b>Belum mempunyai jumlah halaman</b> <br>
+            @endif
+            Translate Judul Buku : {!! $data->translate_judul_buku ?? '<i>Translate Buku Belum Tersedia</i>' !!} <br>
+            <hr>
+            @endforeach
+        </div>
+        <!-- /.card-body -->
+        <div class="card-footer">
+            Footer
+        </div>
+        <!-- /.card-footer-->
+    </div>
+    <!-- /.card -->
+
+</section>
+<!-- /.content -->
+@endsection --}}
